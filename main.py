3dd313da99c7baf0e7ac0a50dcd0f569ed3cadfc
gastorellano <gastorellano@gmail.com>
2024-08-15 11:04:03 -0300

    Ruta corregida

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ app = FastAPI()
 movies_credits = pd.read_csv('DATA/movies_credits.csv') #Se crea un DataFrame para las diferentes consultas.
 
 
+@app.get("/")
+def read_root():
+    return {"message": "Bienvenido a la API de películas. Usa /meses/{mes} para obtener datos de filmaciones."}
 
 
 ## Se ingresa un mes en idioma Español. Debe devolver la cantidad de películas que fueron estrenadas en el mes consultado en la totalidad del dataset.
@@ -45,7 +48,7 @@ def cantidad_filmaciones_mes(mes: str):
     # Contar las películas estrenadas en el mes especificado, que hayan sido estrenadas
     lista_filmaciones = movies_credits[
         (movies_credits['release_date'].dt.month == mes_numero) 
-    #    & (movies_credits['status'] == 'Released')
+        & (movies_credits['status'] == 'Released')
     ]
     
     cantidad_filmaciones = len(lista_filmaciones)
