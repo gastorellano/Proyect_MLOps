b288e896d761fe37c41faad16c4308f23497f211
gastorellano <gastorellano@gmail.com>
2024-08-20 05:14:26 -0300

    depuración dataset sistema recomendación

--- main.py
+++ main.py
@@ -1,10 +1,22 @@
 from fastapi import FastAPI, HTTPException
 import pandas as pd
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+import string 
+from typing import List
+
 
 app = FastAPI()
 app.title = "Buscador de películas"
 movies_credits = pd.read_csv('DATA/movies_credits.csv') #Se crea un DataFrame para las diferentes consultas.
+recomendacion_df = pd.read_parquet('DATA/recomendacion.parquet')
 
+vectorizacion = TfidfVectorizer(stop_words=None)#Se coloca None porque ha sido previamente depurado
+matrizTFIDF = vectorizacion.fit_transform(recomendacion_df['descripcion_combinada'])
+
+#Obtenemos la similitud del coseno
+similitud_coseno = cosine_similarity(matrizTFIDF, matrizTFIDF) 
 
 @app.get("/", tags=['Home'])
 def read_root():
@@ -53,3 +65,33 @@ def cantidad_filmaciones_mes(mes: str):
     cantidad_filmaciones = len(lista_filmaciones) #Guardo en una variable la cantidad de elementos 
     
     return {"mes": mes, "cantidad de peliculas": cantidad_filmaciones} 
+
+
+# Definir la función recomendacion
+def recomendacionPeliculas(title, df=recomendacion_df, similitud_coseno=similitud_coseno):
+    try:
+        indice = df[df['title'].str.lower() == title.lower()].index[0] # Se define el índice de película similar
+        similitud = list(enumerate(similitud_coseno[indice])) 
+        similitud = sorted(similitud, key=lambda x: x[1], reverse=True) #Se obtiene el score de similitud y se ordenan.
+        # Se obtienen las películas similares, dejando afuera la primera(que es el mismo título):
+        similitud = similitud[1:6]
+        indices_similares = [i[0] for i in similitud]
+        peliculas_similares = df['title'].iloc[indices_similares].tolist() #Se crea una lista con las películas
+        mensaje = f"Se encontraron los siguientes títulos similares a '{title}':"
+        print(mensaje)
+        # Retornar la lista de títulos similares
+        return peliculas_similares
+    except IndexError:
+        print(f"No se encontró la película '{title}'. Por favor, intente con otro título.")
+
+@app.get("/recomendacion/{titulo}", response_model=List[str])
+def recomendacion(titulo: str):
+    """
+    Devuelve un listado de cinco películas similares, orientadas según el titulo, genero y descripcion
+    en todo el dataset.
+
+    Recibe un único Parámetro:
+    El título de la película a la que se desea obtener recomendaciones (str)."""
+
+    peliculas_recomendadas = recomendacionPeliculas(titulo, recomendacion_df, similitud_coseno)
+    return peliculas_recomendadas
